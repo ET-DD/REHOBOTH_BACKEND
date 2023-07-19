5482d8553cce1806a48355f861d91d6d8640fbe6
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-07-19 15:07:07 +0300

    created db helper functions for user and note

---
+++ src/utils/db_functions/note.db.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import NoteModel from "../../model/note.model";
+
+export async function getAllFolders() {
+  try {
+    const folders = await NoteModel.find();
+    return folders;
+  } catch (error) {
+    // Handle error
+    console.error("Error retrieving folders:", error);
+    throw error;
+  }
+}
+
+export async function showFolder(id: string) {
+  try {
+    const folder = await NoteModel.findById(id);
+    return folder;
+  } catch (error) {
+    // Handle error
+    console.error("Error retrieving MenuType:", error);
+    throw error;
+  }
+}
+
+export async function getAllNotes(id: string) {
+  try {
+    const Folders = await NoteModel.findById(id);
+    const notes = Folders.note;
+    return notes;
+  } catch (error) {
+    // Handle error
+    console.error("Error retrieving note:", error);
+    throw error;
+  }
+}
+
+export async function showNote(id: string) {
+  try {
+    const note = await NoteModel.findOne({
+      "note._id": new mongoose.Types.ObjectId(id),
+    });
+
+    return note;
+  } catch (error) {
+    // Handle error
+    console.error("Error retrieving note:", error);
+    throw error;
+  }
+}
---
+++ src/utils/db_functions/user.db.ts
@@ -0,0 +1,13 @@
+import UserModel from "../../model/user.model";
+
+export async function getAllUsers() {
+    return await UserModel.find()
+}
+
+export async function showUsersbyId(id: string) {
+    return await UserModel.findOne({_id: id})
+}
+
+export async function showUsersbyEmail(email: string) {
+    return await UserModel.findOne({email: email})
+}
