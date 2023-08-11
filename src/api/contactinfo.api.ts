14f87a6aca5c43d3e27b9cc9509256458fc04170
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-08-11 13:21:05 +0300

    contact info updated

--- src/api/contactinfo.api.ts
+++ src/api/contactinfo.api.ts
@@ -1,15 +1,15 @@
 import express from "express";
 import {
- create, 
- update,
- getContactInfos, 
- getContactInfo, 
- deleteInfo
+    getContactInfos,
+    getContactInfo, 
+    createInfo,
+    update, 
+    deleteInfo
 } from "../controller/contact/index.contact.controller";
 import { authJWT } from "../middleware/authJWT";
 const router = express.Router();
 
-router.post("/create", create);
+router.post("/create", createInfo);
 router.get("/get",  getContactInfos);
 router.get("/show/:id", authJWT, getContactInfo);
 router.put("/update/:id", authJWT, update)
